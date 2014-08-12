a7824f99be1b541f48b9b5f7483db42fdef29d71
Louis Tiao <louistiao@gmail.com>
2014-08-12 21:38:55 +1000

    fixed wsgi

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 nba-stats
 =========
+
+Refer to https://gist.github.com/ltiao/9810719feecccb432b4a for specifics on the structure of this project.
--- nba_stats/nba_stats/wsgi.py
+++ nba_stats/nba_stats/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "<dj_project>.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nba_stats.settings.base")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
